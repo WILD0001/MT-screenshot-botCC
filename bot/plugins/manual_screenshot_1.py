16c3f5fa1dd2b9190626ee4c964a4b99ce5ce5ff
WILD0001 <101416671+WILD0001@users.noreply.github.com>
2022-07-09 13:28:10 +0530

    Update manual_screenshot_1.py

--- bot/plugins/manual_screenshot_1.py
+++ bot/plugins/manual_screenshot_1.py
@@ -16,7 +16,8 @@ async def _(c, m):
         f"#manual_screenshot\n\n{dur}\n\nNow send your list of seconds separated by `,`(comma).\nEg: `0,10,40,60,120`."
         "\nThis will generate screenshots at 0, 10, 40, 60, and 120 seconds. \n\n"
         "1. The list can have a maximum of 10 valid positions.\n"
-        "2. The position has to be greater than or equal to 0, or less than the video length in order to be valid.",
+        "2. The position has to be greater than or equal to 0, or less than the video length in order to be valid."
+        "3. Ith ellam nallonam vaayich nokkeeett undakkaan vanna mathy allel kaalel vaari ban adikkum",
         reply_to_message_id=m.message.reply_to_message.message_id,
         reply_markup=ForceReply(),
     )
