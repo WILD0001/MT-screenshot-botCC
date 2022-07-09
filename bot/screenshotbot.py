06458512f67e226e21af6847a4117e90f32b7977
WILD0001 <101416671+WILD0001@users.noreply.github.com>
2022-07-09 14:58:22 +0530

    Update screenshotbot.py

--- bot/screenshotbot.py
+++ bot/screenshotbot.py
@@ -20,7 +20,6 @@ log = logging.getLogger(__name__)
 class ScreenShotBot(Client):
     def __init__(self):
         super().__init__(
-            session_name = 'Ss_By_hsk_bot',
             bot_token=Config.BOT_TOKEN,
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
